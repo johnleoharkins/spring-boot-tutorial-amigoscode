083dc630cfc69571584fad07122be3d9dec88686
johnleoharkins <johnleoharkins003@gmail.com>
2023-03-02 17:39:38 -0600

    database updates and config

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,8 +1,30 @@
 package com.example.demo.student;
 
+import jakarta.persistence.*;
+
 import java.time.LocalDate;
 
+/**
+ * Student Model
+ *
+ * map the Student class model to our database
+ * @Entity for Hibernate
+ * @Table for table in database
+ *
+ */
+@Entity
+@Table
 public class Student {
+    @Id
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
